c053d9c38125ea9b2a9b354e3b5345ee4ac8a1cf
Manvendra Singh <manvendrasingh@Manvendras-MacBook-Pro.local>
2018-06-21 00:52:13 +0530

    All files commit

---
+++ src/main/java/com/example/demo/CloudPcfApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPcfApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/cloud")
+public class Controller {
+	
+	@RequestMapping("/pcf")
+	public String pivotalCloud() {
+		return "Welcome to pivotal cloud foundry";
+	}
+
+}
